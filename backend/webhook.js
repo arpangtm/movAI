9cbe094bf08da8371dcffa4951a0d054bc2e9008
arpangtm <gautamarpan8@gmail.com>
2025-08-28 12:21:23 -0500

    Changes

--- backend/webhook.js
+++ backend/webhook.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Vite dev server
+    origin: "https://mov-ai.vercel.app/", // Vite dev server
     credentials: true,
   })
 );
